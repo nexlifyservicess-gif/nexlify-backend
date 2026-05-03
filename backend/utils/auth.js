3436bc54a2a6341c0df5b0b2922794f41578f03b
nexlifyservicess-gif <nexlify.servicess@gmail.com>
2026-05-04 01:29:39 +0500

    Delete backend directory

--- backend/utils/auth.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-exports.generateAccessToken = (user) => {
-  return jwt.sign({ id: user._id, role: user.role }, process.env.JWT_ACCESS_SECRET, { expiresIn: '15m' });
-};
-
-exports.generateRefreshToken = (user) => {
-  return jwt.sign({ id: user._id }, process.env.JWT_REFRESH_SECRET, { expiresIn: '7d' });
-};
